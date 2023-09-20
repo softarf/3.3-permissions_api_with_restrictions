1bb62a88d1f2bdbbd8963ff7b8221de04b1f1cff
softarf <98207887+softarf@users.noreply.github.com>
2023-09-20 08:44:10 +0500

    Показывает работу пользовательского фильтра.
    Changes committed:
        1. modified:   advertisements/filters.py
        2. modified:   advertisements/serializers.py
        3. modified:   advertisements/views.py
        4. modified:   api_with_restrictions/settings.py

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,6 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.permissions import IsOwner
 from advertisements.serializers import AdvertisementSerializer
@@ -11,19 +15,24 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     # Разрешения на уровне представления (контроллера).
-    permission_classes = []
+    permission_classes = []    # Добавляются права возвращаемые get_permissions() # IsAdmin
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+
+    filterset_class = AdvertisementFilter
+    #     Точное совпадение по полям. Задаётся внутри класса (Meta.fields=).
+    #     filterset_fields = ['creator', 'title', 'description', 'created_at']
 
     def get_permissions(self):
         """ Выдаёт права на действия.
-            Действия: 'create', 'update', 'partial_update', 'destroy', 'list' и 'retrieve'.
-            Соответствуют: 'POST', 'PUT',     'PATCH',      'DELETE'    'GET' и 'GET.../id/'
+            Действия (action): 'create', 'update', 'partial_update', 'destroy', 'list',  'retrieve'  и 'favourite'.
+            Соответствуют:      'POST',    'PUT',     'PATCH',        'DELETE', 'GET',  'GET.../id/' и 'GET.../fav/'
         """
-        # "Создают только авторизованные"
-        if self.action in ['create']:
+        # "Создавать заметки можно только авторизованным пользователям."
+        if self.action in ['create']:    # , 'favourite'
             return [IsAuthenticated()]
 
-        # "Обновляют и удаляют только авторы"
+        # "Обновлять и удалять можно только свои заметки."
         if self.action in ['update', 'partial_update', 'destroy']:
             return [IsOwner()]
 
-        return []    # "Просматривают все, (согласно глобальной настройки)"
+        return []    # "Просматривают все", согласно глобальной настройки.
